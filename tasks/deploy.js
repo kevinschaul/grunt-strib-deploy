7e0dc04452dd959425d8463d97a2d72b05ab2bd2
Kevin Schaul <kevin.schaul@gmail.com>
2013-11-24 14:39:16 -0600

    Add ensureCorrectGitBranch

--- tasks/deploy.js
+++ tasks/deploy.js
@@ -10,15 +10,19 @@
 
 module.exports = function(grunt) {
 
-  // Please see the Grunt documentation for more information regarding task
-  // creation: http://gruntjs.com/creating-tasks
+  var exec = require('child_process').exec;
 
   grunt.registerTask('deploy', 'Deploy to apps.startribune.com', function() {
+    var done = this.async();
+
     // Merge task-specific and/or target-specific options with these defaults.
     var options = this.options({
       appsSvnPath: '../apps',
       branch: 'master'
     });
+
+    var branch = options.branch;
+    var cwd = process.cwd();
 
     // Require `dateSlug` option
     if (!options.dateSlug) {
@@ -26,8 +30,10 @@ module.exports = function(grunt) {
       return false;
     }
 
-    // TODO
     // Ensure we are on correct git branch
+    ensureCorrectGitBranch(cwd, options.branch, function(not_error) {
+      done(not_error);
+    });
 
     // TODO
     // Ensure all changes have been committed to git
@@ -44,6 +50,41 @@ module.exports = function(grunt) {
     // TODO
     // Commit to svn repository using slug + most recent git message
   });
+
+  var ensureCorrectGitBranch = function(cwd, branch, callback) {
+    var not_error = true;
+
+    var child = exec('git branch', function(error, stdout, stderr) {
+      if (error) {
+        grunt.log.error(error);
+        not_error = false;
+      }
+
+      if (stdout) {
+        var this_branch = getCurrentBranch(stdout);
+        grunt.verbose.write('Current branch is `' + this_branch + '`');
+
+        if (this_branch !== branch) {
+          grunt.log.error('Must deploy from `' + branch + '` branch.');
+          grunt.log.error('(Current branch is `' + this_branch + '`)');
+          not_error = false;
+        }
+      }
+
+      if (callback) {
+        callback(not_error);
+      }
+    });
+  }
+
+  var getCurrentBranch = function(stdout) {
+    var re = /\* ([\S]+)/;
+    var matches = re.exec(stdout);
+    if (matches.length >= 2) {
+      var this_branch = matches[1];
+      return this_branch;
+    }
+  }
 
 };
 
