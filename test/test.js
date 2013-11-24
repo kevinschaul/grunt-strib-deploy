cf6f24c25dc2623f31008f9f8fa6bbb42e910d65
Kevin Schaul <kevin.schaul@gmail.com>
2013-11-24 14:46:53 -0600

    Work on test structure

--- test/test.js
+++ test/test.js
@@ -1,4 +1,20 @@
 'use strict';
 
 var assert = require('assert');
+var deploy = require('../');
+
+describe('ensureCorrectGitBranch', function() {
+  describe('ensureCorrectGitBranch()', function() {
+
+  });
+
+  describe('getCurrentBranch', function() {
+    it('Parses the current branch from the result of `git branch`.', function() {
+      var result = '* master';
+      // TODO
+    });
+
+  });
+
+});
 
