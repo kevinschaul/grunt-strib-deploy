a18efa752656b48bd97e20d2c05e31b00d7a8c27
Kevin Schaul <kevin.schaul@gmail.com>
2014-03-27 10:14:32 -0500

    Add ensure functions (see long message):
    - ensureNoPendingChanges()
    - ensureAppsSvnPathExists()

--- tasks/deploy.js
+++ tasks/deploy.js
@@ -11,6 +11,7 @@
 module.exports = function(grunt) {
 
   var exec = require('child_process').exec;
+  var fs = require('fs');
 
   grunt.registerTask('deploy', 'Deploy to apps.startribune.com', function() {
     var done = this.async();
@@ -30,25 +31,29 @@ module.exports = function(grunt) {
       return false;
     }
 
+    // TODO Get out of callback hell
+
     // Ensure we are on correct git branch
     ensureCorrectGitBranch(cwd, options.branch, function(not_error) {
-      done(not_error);
+
+      // Ensure all changes have been committed to git
+      ensureNoPendingChanges(cwd, function(not_error) {
+
+        // Ensure appsSvnPath exists
+        ensureAppsSvnPathExists(cwd, options.appsSvnPath, function(not_error) {
+
+          // TODO
+          // Ensure local svn repository is up to date
+
+          // TODO
+          // Copy latest project files to local svn repository
+
+          // TODO
+          // Commit to svn repository using slug + most recent git message
+          done(not_error);
+        });
+      });
     });
-
-    // TODO
-    // Ensure all changes have been committed to git
-
-    // TODO
-    // Ensure appsSvnPath exists
-
-    // TODO
-    // Ensure local svn repository is up to date
-
-    // TODO
-    // Copy latest project files to local svn repository
-
-    // TODO
-    // Commit to svn repository using slug + most recent git message
   });
 
   var ensureCorrectGitBranch = function(cwd, branch, callback) {
@@ -69,6 +74,41 @@ module.exports = function(grunt) {
           grunt.log.error('(Current branch is `' + this_branch + '`)');
           not_error = false;
         }
+      }
+
+      if (callback) {
+        callback(not_error);
+      }
+    });
+  }
+
+  var ensureNoPendingChanges = function(cwd, callback) {
+    var not_error = true;
+
+    var child = exec('git status -s', function(error, stdout, stderr) {
+      if (error) {
+        grunt.log.error(error);
+        not_error = false;
+      }
+
+      if (stdout) {
+        grunt.log.error('Please commit your pending changes.');
+        not_error = false;
+      }
+
+      if (callback) {
+        callback(not_error);
+      }
+    });
+  }
+
+  var ensureAppsSvnPathExists = function(cwd, appsSvnPath, callback) {
+    var not_error = true;
+
+    fs.exists(appsSvnPath, function(exists) {
+      if (!exists) {
+        grunt.log.error('Please correct your `appsSvnPath`.');
+        not_error = false;
       }
 
       if (callback) {
